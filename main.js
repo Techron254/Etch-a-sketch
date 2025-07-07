6fc01e304c22749f2ae39ac4a0718cf218a10809
Techron254 <leonardronald04@gmail.com>
2025-07-07 19:17:15 +0300

    make the canvas subdividable as per the user input

--- main.js
+++ main.js
@@ -3,17 +3,29 @@ let canvasWidth = 400;
 let canvasHeight = 400;
 canvas.style.width = `${canvasWidth}px`;
 canvas.style.height = `${canvasHeight}px`;
-let cellsNumber = 10;
-
+let cellsNumber;
+cellsNumber = 16;
 function CreateGridCells(cellsNumber){
 
     for (let i = 1; i <= cellsNumber * cellsNumber; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         canvas.appendChild(cell);
-        cell.style.cssText = 'border: 0.5px solid gray; flex-grow: 1';
-        cell.style.width = `${(canvasWidth/cellsNumber)-1}px`;
+        cell.style.cssText = 'border: 0.5px solid gray; flex-grow: 1; flex-shrink: 1';
+        cell.style.width = `${(canvasWidth/cellsNumber)}px`;
         cell.style.height = `${(canvasHeight/cellsNumber)}px`;
         }
 }
-  CreateGridCells(16);
+ CreateGridCells(cellsNumber);
+
+
+  const createNewGridButton = document.querySelector('#button-one');
+  createNewGridButton.addEventListener('click', () => {
+    cellsNumber = prompt('Enter the number of squares you wish per side for the new grid (max: 100)');
+    let allCells = canvas.querySelectorAll('div');
+    allCells.forEach((div) => div.remove());
+    CreateGridCells(cellsNumber)});
+
+
+
+
