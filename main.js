c0ecc21f50cb5e3796582109e91e83b7f00be27f
Techron254 <leonardronald04@gmail.com>
2025-07-09 15:02:38 +0300

    add erase property and hover button

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('.canvas');
-let canvasWidth = 400;
-let canvasHeight = 400;
+let canvasWidth = 500;
+let canvasHeight = 500;
 canvas.style.width = `${canvasWidth}px`;
 canvas.style.height = `${canvasHeight}px`;
 let cellsNumber;
@@ -10,14 +10,12 @@ function CreateGridCells(cellsNumber){
     for (let i = 1; i <= cellsNumber * cellsNumber; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        cell.style.cssText = 'border: 1px solid rgba(129, 124, 124, 0.77); flex-grow: 1; flex-shrrrink: 1; box-sizing: border-box;';
+        cell.style.cssText = 'border: 1px solid rgba(63, 62, 62, 0.77); flex-grow: 1; flex-shrrrink: 1; box-sizing: border-box;';
         cell.style.width = `${((1/cellsNumber) * 100)}%`;
         cell.style.height = `${((1/cellsNumber)*100)}%`;
+        cell.style.backgroundColor ='rgba(0, 0, 0, 0.998)';
         canvas.appendChild(cell);
-
-        cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'rgb(115, 190, 115)';
-        })
+        hover();
 
     }
 }
@@ -25,24 +23,53 @@ function CreateGridCells(cellsNumber){
  CreateGridCells(cellsNumber);
 
 
-const createNewGridButton = document.querySelector('#button-one');
+ function hover() {
+  let allCells = canvas.querySelectorAll('div');
+    allCells.forEach((cell) =>
+    {
+    cell.addEventListener('mouseenter', () => {
+        cell.style.backgroundColor = 'rgb(115, 190, 115)';
+        })
+    });
+  }
+
+const createNewGridButton = document.querySelector('#create-grid');
   createNewGridButton.addEventListener('click', () => {
     cellsNumber = prompt('Enter the number of squares you wish per side for the new grid (max: 100)');
 
-    if (isNaN(cellsNumber) || cellsNumber >100 || cellsNumber < 1){
+    if (isNaN(cellsNumber) || cellsNumber > 100 || cellsNumber < 1){
       alert("Please input a number between 1 to 100");
     }
     else {
-    let allCells = canvas.querySelectorAll('div');
+    // let allCells = canvas.querySelectorAll('div');
     allCells.forEach((div) => div.remove());
+
     CreateGridCells(cellsNumber);
   }
   });
 
-const resetGridButton = document.querySelector('#button-two');
+const resetGridButton = document.querySelector('#reset-button');
   resetGridButton.addEventListener('click', () => {
      let allCells = canvas.querySelectorAll('div');
       allCells.forEach((div) => div.remove());
 
       CreateGridCells(cellsNumber);
     });
+
+const eraserButton = document.querySelector('#eraser');
+  eraserButton.addEventListener('click', e =>  erase());
+
+function erase(){
+    let allCells = canvas.querySelectorAll('div');
+    allCells.forEach((cell) =>{
+        cell.removeEventListener('mouseenter', () => {
+        cell.style.backgroundColor = 'rgb(115, 190, 115)';
+        });
+    cell.addEventListener('mouseenter', () => {
+      if (cell.style.backgroundColor != "rgba(0, 0, 0, 0.998)"){
+        cell.style.backgroundColor ='rgba(0, 0, 0, 0.998)';
+}})});
+}
+
+const hoverButton = document.querySelector('#toggle-button');
+hoverButton.addEventListener('click', e => hover());
